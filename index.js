707514405d3200071cadd061856050a47055d3a1
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-28 14:36:41 +0000

    improvement

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.get('/', (req, res) => {
   axios.get("https://game8.co/games/Genshin-Impact/archives/297500").then(response => {
     const $ = cheerio.load(response.data);
     jsons = {
-      "status" : "current",
+      "bannerStatus" : "current",
       "title" : $("h3.a-header--3").first().text(),
       "content" : {
-        "banner1" : $("p.a-paragraph").find('img[alt="Genshin Impact - 4.6 Phase 1 Banner"]').attr("data-src"),
-        "banner2" : $("p.a-paragraph").find("img[alt='Genshin - Epitome Banner 4.6 Phase 2']").attr("data-src")
+        "banner1" : $("p.a-paragraph").find('img').eq(1).attr("data-src"),
+        "banner2" : $("p.a-paragraph").find("img").eq(2).attr("data-src"),
        }
       }
     res.json(jsons)
