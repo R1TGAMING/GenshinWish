0609f328c7bb2a3d36c19340a2923ef397fedcfe
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-28 10:13:38 +0000

    0.1 update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({"hello" : "world"})
+  axios.get("https://game8.co/games/Genshin-Impact/archives/297500").then(response => {
+    const $ = cheerio.load(response.data);
+    jsons = {
+      "status" : "current",
+      "title" : $("h3.a-header--3").first().text(),
+      "content" : {
+        "banner1" : $("p.a-paragraph").find('img[alt="Genshin Impact - 4.6 Phase 1 Banner"]').attr("data-src"),
+        "banner2" : $("p.a-paragraph").find("img[alt='Genshin - Epitome Banner 4.6 Phase 2']").attr("data-src")
+       }
+      }
+    res.json(jsons)
+  })
 })
 
 app.listen(8000, () => {
