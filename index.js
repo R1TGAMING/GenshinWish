5a27e62a1c6ffd644ed75a0cb1c15dd24d3ff38f
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-06-03 13:23:36 +0000

    update Character Up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/genshinWish', (req, res) => {
   axios.get("https://game8.co/games/Genshin-Impact/archives/297500").then(response => {
     const $ = cheerio.load(response.data);
     jsons = {
@@ -13,6 +13,21 @@ app.get('/', (req, res) => {
       "content" : {
         "banner1" : $("p.a-paragraph").find('img').eq(1).attr("data-src"),
         "banner2" : $("p.a-paragraph").find("img").eq(2).attr("data-src"),
+        "characterRateUp" : {
+          "nameBanner1" : $("a.a-link").find("img").attr("alt"),
+          "imageBanner1" : $("a.a-link").find("img").attr("data-src"),
+          "nameBanner2" : $("a.a-link").find("img").eq(1).attr("alt"),
+          "imageBanner2" : $("a.a-link").find("img").eq(1).attr("data-src"),
+        },
+        "4starCharacterRateUp" : {
+          "name" : $("a.a-link").find("img").eq(2).attr("alt"),
+          "image" : $("a.a-link").find("img").eq(2).attr("data-src"),
+          "name2" : $("a.a-link").find("img").eq(3).attr("alt"),
+          "image2" : $("a.a-link").find("img").eq(3).attr("data-src"),
+          "name3" : $("a.a-link").find("img").eq(4).attr("alt"),
+          "image3" : $("a.a-link").find("img").eq(4).attr("data-src")
+        }
+        
        }
       }
     res.json(jsons)
